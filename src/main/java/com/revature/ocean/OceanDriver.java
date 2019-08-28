5bf51a6653c5eb9c37edcc0f5476aa03082d1231
Efrain Vila <efrainvila@gmail.com>
2019-08-28 11:39:52 -0400

    update082819 1139

--- src/main/java/com/revature/ocean/OceanDriver.java
+++ src/main/java/com/revature/ocean/OceanDriver.java
@@ -1,6 +1,7 @@
 package com.revature.ocean;
 
-import java.io.ObjectInputStream.GetField;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Domain planned: Ocean 
@@ -43,8 +44,29 @@ public class OceanDriver {
 		//static variable
 		System.out.println(Shark.population);
 		
+		//collection mode
+		sharkHerd();
 		
-
+		
 	}
+
+	private static void sharkHerd() {
+		List<Shark> sharkList = new ArrayList<Shark>();
+		sharkList.add(new Shark("Jon", "8 feet 7 inches", 373));
+		sharkList.add(new Shark("Chancellor", "6 feet 4 inches", 4436));
+		sharkList.add(new Shark("Gareth", "10 feet 4 inches"));
+		sharkList.add(new Shark("Sero", "11 feet 2 inches"));
+		
+		for(Shark sharkGuide : sharkList) {
+			System.out.println(sharkGuide);
+		}
+		
+		//System.out.println(sharkList);
+		
+		
+		
+	}
+
+	
 
 }
